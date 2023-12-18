dd9594dc9bb36e288b91bece3e7d07764dcd85aa
hanjae <leehanjae99@gmail.com>
2023-12-18 22:54:38 +0900

    spring boot prac 231218

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/PackmanGame.java
@@ -0,0 +1,24 @@
+package com.in28minutes.learnspringframework.game;
+
+public class PackmanGame implements GamingConsole{
+
+    @Override
+    public void up() {
+        System.out.println("Packman Up!");
+    }
+
+    @Override
+    public void down() {
+        System.out.println("Packman Down!");
+    }
+
+    @Override
+    public void left() {
+        System.out.println("Packman move Left!");
+    }
+
+    @Override
+    public void right() {
+        System.out.println("Packman move Right!");
+    }
+}
